bd5cf282c45b308fd806c5a97e272e96d8f320c4
khanh.nguyen <khanh.nguyen@knorex.com>
2024-02-04 22:52:22 +0700

    fix bug in unet architecture

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,6 +1,67 @@
 import torch
 import torch.nn as nn
 
+
+class SPConvTranspose2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, r=1):
+        super(SPConvTranspose2d, self).__init__()
+        self.pad1 = nn.ConstantPad2d((1, 1, 0, 0), value=0.)
+        self.out_channels = out_channels
+        self.conv = nn.Conv2d(in_channels, out_channels * r, kernel_size=kernel_size, stride=(1, 1))
+        self.r = r
+
+    def forward(self, x):
+        x = self.pad1(x)
+        out = self.conv(x)
+        batch_size, nchannels, H, W = out.shape
+        out = out.view((batch_size, self.r, nchannels // self.r, H, W))
+        out = out.permute(0, 2, 3, 4, 1)
+        out = out.contiguous().view((batch_size, nchannels // self.r, H, -1))
+        return out
+
+class MaskDecoder(nn.Module):
+    def __init__(self, num_features, num_channel=3, out_channel=1):
+        super(MaskDecoder, self).__init__()
+        self.sub_pixel = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, num_channel, kernel_size=2, stride=2, output_padding=1)
+        )
+        self.conv_1 = nn.Conv2d(num_channel, out_channel, (1, 1))
+        self.norm = nn.InstanceNorm2d(out_channel, affine=True)
+        self.prelu = nn.PReLU(out_channel)
+        self.final_conv = nn.Conv2d(out_channel, out_channel, (1, 1))
+        self.prelu_out = nn.PReLU(num_features, init=-0.25)
+
+    def forward(self, x):
+        x = self.sub_pixel(x)
+        x = self.conv_1(x)
+        x = self.prelu(self.norm(x))
+        x = self.final_conv(x).permute(0, 3, 2, 1).squeeze(-1)
+        return self.prelu_out(x).permute(0, 2, 1).unsqueeze(1)
+    
+class ComplexDecoder(nn.Module):
+    def __init__(self, num_channel=3):
+        super(ComplexDecoder, self).__init__()
+        self.sub_pixel = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, num_channel, kernel_size=2, stride=2, output_padding=1)
+        )
+        self.prelu = nn.PReLU(num_channel)
+        self.norm = nn.InstanceNorm2d(num_channel, affine=True)
+        self.conv = nn.Conv2d(num_channel, 2, (1, 1))
+
+    def forward(self, x):
+        x = self.sub_pixel(x)
+        x = self.prelu(self.norm(x))
+        x = self.conv(x)
+        return x
+    
 class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UNet, self).__init__()
@@ -24,13 +85,9 @@ class UNet(nn.Module):
         )
 
         # Decoder
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(64, out_channels, kernel_size=2, stride=2)
-        )
+        # Decoder
+        self.mask_decoder = MaskDecoder(201, num_channel=3, out_channel=1)
+        self.complex_decoder = ComplexDecoder(num_channel=3)
 
     def forward(self, x):
         mag = torch.sqrt(x[:, 0, :, :]**2 + x[:, 1, :, :]**2).unsqueeze(1)
@@ -44,9 +101,16 @@ class UNet(nn.Module):
         x2 = self.mid(x1)
 
         # Decoder
-        x3 = self.decoder(x2)
+        mask = self.mask_decoder(x2)
+        out_mag = mask * mag
 
-        return x3
+        complex_out = self.complex_decoder(x2)
+        mag_real = out_mag * torch.cos(noisy_phase)
+        mag_imag = out_mag * torch.sin(noisy_phase)
+        final_real = mag_real + complex_out[:, 0, :, :].unsqueeze(1)
+        final_imag = mag_imag + complex_out[:, 1, :, :].unsqueeze(1)
+
+        return final_real, final_imag
 
 if __name__ == "__main__":
     # Example usage
